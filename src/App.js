e207b773eb83255f27592740b39ac0b9d83296b9
Andrew Rizzo <rizzoa256@gmail.com>
2024-01-06 16:58:41 -0500

    move comps playlist and tarcklist and tack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+
+// import Playlist from './components/Playlist/Playlist';
 import SearchBar from './components/SearchBar/SearchBar';
 import SearchResults from './components/SearchResults/SeachResults';
 
