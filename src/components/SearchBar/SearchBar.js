3ebc842fe44a4d4a88963f9f2fc12da6425f0e1b
Andrew Rizzo <rizzoa256@gmail.com>
2024-01-06 15:50:10 -0500

    add searchbar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBar = (props) => {
+    return (
+        <div>
+     <input placeholder="Enter A Song Title" />
+      <button> 
+      SEARCH
+      </button>
+
+        </div>
+    );
+};
+
+export default SearchBar;
