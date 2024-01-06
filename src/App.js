e71f7f4ed51790fbc3a67ff25b8927c2384cd41a
Andrew Rizzo <rizzoa256@gmail.com>
2024-01-06 16:11:59 -0500

    add rerult compont

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
+import SearchResults from './components/SearchResults/SeachResults';
+
+
 function App() {
   return (
     <div className="App">
+      <h1>jmmmming</h1>
+    <div>
       <SearchBar />
-
+    <div>
+      <SearchResults />
+      
+          </div>
+      </div> 
     </div>
   );
 }
---
+++ src/components/SearchResults/SeachResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchResults = () => {
+    return (
+        <div>
+     <h2>Results</h2>
+
+        </div>
+    );
+};
+
+export default SearchResults;
